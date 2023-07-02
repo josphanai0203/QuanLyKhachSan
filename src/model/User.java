013c71d3c8c9a740b729ca632300570b5cf9ed8b
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2023-07-02 22:31:41 +0700

    user changes

--- src/model/User.java
+++ src/model/User.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
-
-/**
- *
- * @author Admin
- */
 public class User {
+    private String account;
+    private String password;
+    private boolean isAdmin;
+
+    public User() {
+    }
+
+    public User(String account, String password, boolean isAdmin) {
+        this.account = account;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+    
     
 }
