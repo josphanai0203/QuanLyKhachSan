672e4ad3c5b01e70dfb4327453305a26839f469f
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2023-07-03 13:59:19 +0700

    create dao (UserDAO and DAOInterface)

--- src/model/Staff.java
+++ src/model/Staff.java
@@ -4,10 +4,89 @@
  */
 package model;
 
+import java.util.*;
+
 /**
  *
  * @author Admin
  */
 public class Staff {
+    private String maNhanVien;
+    private  String chucVu;
+    private String hoVaTen;
+    private String sdt;
+    private Date ngayThangNamSinh;
+    private double luong;
+    private String khuVuc;
+
+    public Staff() {
+    }
+
+    public Staff(String maNhanVien, String chucVu, String hoVaTen, String sdt, Date ngayThangNamSinh, double luong, String khuVuc) {
+        this.maNhanVien = maNhanVien;
+        this.chucVu = chucVu;
+        this.hoVaTen = hoVaTen;
+        this.sdt = sdt;
+        this.ngayThangNamSinh = ngayThangNamSinh;
+        this.luong = luong;
+        this.khuVuc = khuVuc;
+    }
+
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public String getHoVaTen() {
+        return hoVaTen;
+    }
+
+    public void setHoVaTen(String hoVaTen) {
+        this.hoVaTen = hoVaTen;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public Date getNgayThangNamSinh() {
+        return ngayThangNamSinh;
+    }
+
+    public void setNgayThangNamSinh(Date ngayThangNamSinh) {
+        this.ngayThangNamSinh = ngayThangNamSinh;
+    }
+
+    public double getLuong() {
+        return luong;
+    }
+
+    public void setLuong(double luong) {
+        this.luong = luong;
+    }
+
+    public String getKhuVuc() {
+        return khuVuc;
+    }
+
+    public void setKhuVuc(String khuVuc) {
+        this.khuVuc = khuVuc;
+    }
+    
     
 }
