c127fe28cea6c2f0b7b11b08897b3a2662337f97
anhpink <anhhong11062003qn@gmail.com>
2023-07-06 00:14:08 +0700

    Hong push

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Admin
- */
 public class Room {
-    
+    private int id_room;
+    private String name;
+    private double area;
+    private int max_people;
+    private int id_room_type;
+    private int id_rent_type;
+
+    public Room(int id_room, String name, double area, int max_people, int id_room_type, int id_rent_type){
+        this.id_room = id_room;
+        this.name = name;
+        this.area = area;
+        this.max_people = max_people;
+        this.id_room_type = id_room_type;
+        this.id_rent_type = id_rent_type;
+    }
+
+    public int getId_room() {
+        return id_room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getMax_people() {
+        return max_people;
+    }
+
+    public int getId_room_type() {
+        return id_room_type;
+    }
+
+    public int getId_rent_type() {
+        return id_rent_type;
+    }
+
+    public void setId_room(int id_room) {
+        this.id_room = id_room;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setMax_people(int max_people) {
+        this.max_people = max_people;
+    }
+
+    public void setId_room_type(int id_room_type) {
+        this.id_room_type = id_room_type;
+    }
+
+    public void setId_rent_type(int id_rent_type) {
+        this.id_rent_type = id_rent_type;
+    }
 }
