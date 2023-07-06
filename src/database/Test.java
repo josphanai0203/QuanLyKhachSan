3495ea000d0a9e87aea82eaebaf30fd530c93030
longvoquyhoang <127716758+longvoquy@users.noreply.github.com>
2023-07-06 17:16:37 +0700

    Update Test.java
    xoa billout

--- src/database/Test.java
+++ src/database/Test.java
@@ -6,7 +6,7 @@ import model.*;
 public class Test {
 
     public static void main(String[] args) {
-        Service s1 = new Service("Nuoc", 1, 10, 10000.0, 1, 15000.0);
+        Service s1 = new Service("Nuoc", 1, 10, 10000.0, 1);
         ItemServiceDAO.getInstance().add(s1);
     }
 }
