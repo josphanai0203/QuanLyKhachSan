0795f1f84c7af8c9c67b9ad06f1da997c38964b8
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2023-07-06 18:33:25 +0700

    Customer changes

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 import model.Customer;
 import service.ICustomerService;
 import database.JDBCUtil;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Staff;
 
 /**
  *
@@ -17,12 +23,44 @@ public class CustomerDAO implements ICustomerService {
 
     @Override
     public boolean add(Customer t) {
+
         return true;
     }
 
     @Override
     public ArrayList<Customer> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ArrayList<Customer> kq = new ArrayList<>();
+        try {
+            Connection con = JDBCUtil.getConnection();
+            String sql = "SELECT * FROM khach_hang";
+            PreparedStatement st = con.prepareStatement(sql);
+            //b3: thuc thi cau lenh sql  
+            System.out.println("Ban da thuc thi: " + sql);
+
+            ResultSet rs = st.executeQuery();
+
+            //b4: xu li 
+            while (rs.next()) {
+                int ma_khach_hang = rs.getInt("ma_khach_hang");
+                String ten_khach_hang = rs.getString("ten_khach_hang");
+                int nam_sinh = rs.getInt("nam_sinh");
+                String gioi_tinh = rs.getString("gioi_tinh");
+                String dia_chi = rs.getString("dia_chi");
+                String quoc_tich = rs.getString("quoc_tich");
+                int so_cmnd = rs.getInt("so_cmnd");
+                String so_dien_thoai = rs.getString("so_dien_thoai");
+
+                Customer c1 = new Customer(ma_khach_hang, ten_khach_hang, nam_sinh, gioi_tinh, dia_chi, quoc_tich, so_cmnd, so_dien_thoai);
+                kq.add(c1);
+            }
+            JDBCUtil.closeConnection(con);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return kq;
+
     }
 
     @Override
