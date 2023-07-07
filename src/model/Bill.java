44adaca10d61461572fe3acf62867ec50ede9e8a
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2023-07-07 13:20:44 +0700

    Payroll, DetailedBill, RegistrationFrom create

--- src/model/Bill.java
+++ src/model/Bill.java
@@ -4,10 +4,79 @@
  */
 package model;
 
+import java.util.Date;
+
 /**
  *
  * @author Admin
  */
 public class Bill {
+    private int maHoaDon;
+    private int maKhachHang;
+    private int maNhanVien;
+    private Date ngayNhap;
+    private double tongTien;
+    private int maHoaDonChiTiet;
+
+    public Bill() {
+    }
+
+    public Bill(int maHoaDon, int maKhachHang, int maNhanVien, Date ngayNhap, double tongTien, int maHoaDonChiTiet) {
+        this.maHoaDon = maHoaDon;
+        this.maKhachHang = maKhachHang;
+        this.maNhanVien = maNhanVien;
+        this.ngayNhap = ngayNhap;
+        this.tongTien = tongTien;
+        this.maHoaDonChiTiet = maHoaDonChiTiet;
+    }
+
+    public int getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public void setMaHoaDon(int maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    public int getMaKhachHang() {
+        return maKhachHang;
+    }
+
+    public void setMaKhachHang(int maKhachHang) {
+        this.maKhachHang = maKhachHang;
+    }
+
+    public int getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(int maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public Date getNgayNhap() {
+        return ngayNhap;
+    }
+
+    public void setNgayNhap(Date ngayNhap) {
+        this.ngayNhap = ngayNhap;
+    }
+
+    public double getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(double tongTien) {
+        this.tongTien = tongTien;
+    }
+
+    public int getMaHoaDonChiTiet() {
+        return maHoaDonChiTiet;
+    }
+
+    public void setMaHoaDonChiTiet(int maHoaDonChiTiet) {
+        this.maHoaDonChiTiet = maHoaDonChiTiet;
+    }
+    
     
 }
